dd472342e81d6a903f328a26ef55f81b30a71083
tien nguyen <nguyentien9969@gmail.com>
2022-10-04 19:39:57 +0700

    first commit

--- assignment1/Program.cs
+++ assignment1/Program.cs
@@ -4,9 +4,115 @@ using System.Text;
 
 namespace Assignment1
 {
-    
     class Program
     {
+        static void GetMaleMember(List<Member> memList)
+        {
+
+            Console.WriteLine("1. Return a list of members who is Male");
+
+            foreach (Member mem in memList)
+            {
+                if (string.Equals(mem.Gender , "male" , StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine(mem.Info);
+                }
+            }
+        }
+
+        static void GetOldestMember(List<Member> memList)
+        {
+
+            uint oldestMemberAge = 0;
+
+            Console.WriteLine("2. Return the oldest one based on “Age”");
+
+            foreach (Member mem in memList)
+            {
+                if (mem.Age > oldestMemberAge)
+                {
+                    oldestMemberAge = mem.Age;
+                }
+            }
+
+            foreach (Member mem in memList)
+            {
+                if (mem.Age == oldestMemberAge)
+                {
+                    Console.WriteLine(mem.Info);
+                    break;
+                }
+            }
+        }
+
+        static void GetFullnameList(List<Member> memList)
+        {
+            Console.WriteLine("3. Return a new list that contains Full Name only ( Full Name =Last Name + FirstName)");
+
+            foreach (Member mem in memList)
+            {
+                Console.WriteLine(mem.FullName);
+            }
+        }
+
+        static void GetAgeList(List<Member> memList)
+        {
+            Console.WriteLine("4. Return 3 lists:");
+
+            Console.WriteLine("Nhập list bạn muốn chọn : ");
+            Console.WriteLine("List người sinh năm 2000 ");
+            Console.WriteLine("List người sinh trước 2000");
+            Console.WriteLine("List người sinh sau 2000 ");
+
+            int option = System.Int32.Parse(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    foreach (var mem in memList)
+                    {
+                        if (mem.DateOfBirth.Year == 2000)
+                        {
+                            Console.WriteLine(mem.Info + " was born in 2000 .");
+                        }
+                    }
+                    break;
+                case 2:
+
+                    foreach (var mem in memList)
+                    {
+                        if (mem.DateOfBirth.Year > 2000)
+                        {
+                            Console.WriteLine(mem.Info + " was born before 2000 .");
+                        }
+                    }
+                    break;
+
+                case 3:
+                    foreach (var mem in memList)
+                    {
+                        if (mem.DateOfBirth.Year < 2000)
+                        {
+                            Console.WriteLine(mem.Info + " was born after 2000 .");
+                        }
+                    }
+                    break;
+            }
+        }
+
+        static void GetfirstBorn(List<Member> memList)
+        {
+            Console.WriteLine("5. Return the first person who was born in Ha Noi.");
+
+            var firstHanoi = 0;
+
+            while (!(string.Equals(memList[firstHanoi].BirthPlace, "Hanoi", StringComparison.OrdinalIgnoreCase)))
+            {
+                firstHanoi++;
+            }
+            Console.WriteLine(memList[firstHanoi].Info);
+        }
+
         static void Main(string[] args)
         {
             List<Member> memList = new List<Member>();
@@ -18,38 +124,30 @@ namespace Assignment1
             memList.Add(new Member("Ha", "Nguyen", "female", new DateTime(1998, 10, 5), 0834467801, "Hue", "false"));
             memList.Add(new Member("Meo", "Con", "uknown", new DateTime(2020, 10, 5), 020, "a", "false"));
 
-            
-            var anoymus = new Member();
-
             Console.OutputEncoding = Encoding.Unicode;
             Console.InputEncoding = Encoding.Unicode;
-
+            
             Console.WriteLine("Chọn List danh sách : ");
 
             int getList = System.Int32.Parse(Console.ReadLine());
-            
+
             switch (getList)
             {
                 case 1:
-                    anoymus.GetMaleMember(memList);
+                    GetMaleMember(memList);
                     break;
-
                 case 2:
-                    anoymus.GetOldestMember(memList);
+                    GetOldestMember(memList);
                     break;
-
                 case 3:
-                    anoymus.GetFullnameList(memList);
+                    GetFullnameList(memList);
                     break;
-
                 case 4:
-                    anoymus.GetAgeList(memList);
+                    GetAgeList(memList);
                     break;
-
                 case 5:
-                    anoymus.GetfirstBorn(memList);
+                    GetfirstBorn(memList);
                     break;
-                    
                 default:
                     Console.WriteLine("không có danh sách bạn muốn tìm");
                     break;
--- assignment1/member.cs
+++ assignment1/member.cs
@@ -6,29 +6,30 @@ namespace Assignment1
     public class Member
     {
 
-        private string _FirstName;
-        private string _LastName;
-        private string _Gender;
-        private DateTime _DateOfBirth;
-        private int _PhoneNumber;
-        private string _BirthPlace;
-        private uint _Age;
-        private string _IsGraduated;
+        private string _firstName;
+        private string _lastName;
+        private string _gender;
+        private DateTime _dateOfBirth;
+        private int _phoneNumber;
+        private string _birthPlace;
+        private uint _age;
+        private string _isGraduated;
 
-        public string FirstName { get => _FirstName; set => _FirstName = value; }
-        public string LastName { get => _LastName; set => _LastName = value; }
-        public string Gender { get => _Gender; set => _Gender = value; }
-        public int PhoneNumber { get => _PhoneNumber; set => _PhoneNumber = value; }
-        public DateTime DateOfBirth { get => _DateOfBirth; set => _DateOfBirth = value; }
-        public string BirthPlace { get => _BirthPlace; set => _BirthPlace = value; }
-        public uint Age { get => _Age; set => _Age = value; }
-        public string IsGraduated { get => _IsGraduated; set => _IsGraduated = value; }
-       
+        public string FirstName { get => _firstName; set => _firstName = value; }
+        public string LastName { get => _lastName; set => _lastName = value; }
+        public string Gender { get => _gender; set => _gender = value; }
+        public int PhoneNumber { get => _phoneNumber; set => _phoneNumber = value; }
+        public DateTime DateOfBirth { get => _dateOfBirth; set => _dateOfBirth = value; }
+        public string BirthPlace { get => _birthPlace; set => _birthPlace = value; }
+        public uint Age { get => _age; set => _age = value; }
+        public string IsGraduated { get => _isGraduated; set => _isGraduated = value; }
+
         public Member()
         {
         }
 
-        public Member(string firstName, string lastName, string gender, DateTime dateOfBirth, int phoneNumber, string birthPlace, string isGraduated)
+        public Member(string firstName, string lastName, string gender, DateTime dateOfBirth,
+                        int phoneNumber, string birthPlace, string isGraduated)
         {
             FirstName = firstName;
             LastName = lastName;
@@ -40,23 +41,26 @@ namespace Assignment1
             Age = this.GetAge();
         }
 
-        public string Info { get => 
-                             " ( " + FirstName + " , " + LastName + " ,  " + Age + " , " + Gender + " , " + DateOfBirth +
-                             " , " + BirthPlace + " , " + PhoneNumber + " , " + Age + " , " + IsGraduated + " )";
-                            }
-        public string FullName {get => 
-                                FirstName + " " + LastName ;
-                                }
-        
-         public uint GetAge()
+        public string Info
+        {
+            get => " ( " + FirstName + " , " + LastName + " ,  " + Age + " , " + Gender + " , " + DateOfBirth +
+             " , " + BirthPlace + " , " + PhoneNumber + " , " + Age + " , " + IsGraduated + " )";
+        }
+
+        public string FullName
+        {
+            get => FirstName + " " + LastName;
+        }
+
+        public uint GetAge()
         {
             uint today = (uint)DateTime.Today.Year;
             return today - (uint)DateOfBirth.Year;
         }
 
-        public string GetIsGraduated(string isGraduated) 
+        public string GetIsGraduated(string isGraduated)
         {
-            if (string.Equals(isGraduated,"true",StringComparison.OrdinalIgnoreCase))
+            if (string.Equals(isGraduated, "true", StringComparison.OrdinalIgnoreCase))
             {
                 return "ĐÃ TỐT NGHIỆP";
             }
@@ -66,136 +70,5 @@ namespace Assignment1
             }
         }
 
-        public void GetMaleMember(List<Member> memList)
-        {
-
-            Console.WriteLine("1. Return a list of members who is Male");
-
-                foreach (Member mem in memList)
-                {
-                    if (mem.Gender == "male")
-                    {
-                        Console.WriteLine(mem.Info);
-                    }
-                }
-        }
-       
-       public void GetOldestMember (List<Member> memList)
-       {
-
-            uint oldestMemberAge = 0;
-
-            Console.WriteLine("2. Return the oldest one based on “Age”");
-
-            foreach (Member mem in memList)
-            {
-                if (mem.Age > oldestMemberAge)
-                {
-                    oldestMemberAge = mem.Age;
-                }
-            }
-
-            foreach (Member mem in memList)
-            {
-                if (mem.Age == oldestMemberAge)
-                {
-                    Console.WriteLine(mem.Info);
-                    break;
-                }
-            }
-       }
-
-       public void GetFullnameList(List<Member> memList)
-       {
-            Console.WriteLine("3. Return a new list that contains Full Name only ( Full Name =Last Name + FirstName)");
-
-            foreach (Member mem in memList)
-            {
-                Console.WriteLine(mem.FullName);
-            }
-       }
-
-       public void GetAgeList(List<Member> memList)
-       {
-         Console.WriteLine("4. Return 3 lists:");
-
-            Console.WriteLine("Nhập list bạn muốn chọn : ");
-            Console.WriteLine("List người sinh năm 2000 ");
-            Console.WriteLine("List người sinh trước 2000");
-            Console.WriteLine("List người sinh sau 2000 ");
-            
-            int option = System.Int32.Parse(Console.ReadLine());
-            
-                switch (option)
-                {
-                    case 1:
-                        foreach (var mem in memList)
-                        {
-                            if(mem.DateOfBirth.Year == 2000)
-                            {
-                                Console.WriteLine(mem.Info + " was born in 2000 .");
-                            }
-                        }
-                    break;
-                    case 2:
-                    
-                       foreach (var mem in memList)
-                        {
-                            if(mem.DateOfBirth.Year > 2000)
-                            {
-                                Console.WriteLine(mem.Info + " was born before 2000 .");
-                            }
-                        }
-                        break;
-                  
-                    case 3:
-                       foreach (var mem in memList)
-                        {
-                            if(mem.DateOfBirth.Year < 2000)
-                            {
-                                Console.WriteLine(mem.Info + " was born after 2000 .");
-                            }
-                        }
-                    break;  
-                }
-       }
-
-       public void GetfirstBorn(List<Member> memList)
-       {
-            Console.WriteLine("5. Return the first person who was born in Ha Noi.");
-
-            var firstHanoi = 0;
-
-            while (memList[firstHanoi].BirthPlace != "Hanoi")
-            {
-                firstHanoi++;
-            }
-            Console.WriteLine(memList[firstHanoi].Info);
-       }
-            
-
-        //  public enum Genders
-        //  {
-        //     male,
-        //     female,
-        //     unknown
-        // }
-
-        // Genders gender = Genders.male;
-
-        // public string GetGender (Genders gender){
-        //     switch (gender) 
-        //     {
-        //         case Genders.male:
-        //         return "Male";
-        //         case Genders.female:
-        //         return "Female";
-        //         case Genders.unknown:
-        //         return "Other";
-        //         default:
-        //         return "wrong input type Gender";
-        //     }
-        // }
     }
-
 }
